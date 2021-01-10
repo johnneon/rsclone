531db92905fe14556d04246a48ba0a93c48fb42a
johnneon <neon.1598@mail.ru>
2021-01-10 20:29:30 +0600

    feat: add authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,41 @@
-import React from 'react';
+import React, { } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import AuthContext from './context/AuthContext';
 import useRouts from './routes';
+import useAuth from './hooks/auth.hook';
 
 function App() {
+  const {
+    token,
+    login,
+    logout,
+    userId,
+    ready,
+  } = useAuth();
   /*
     Тут пока нету бэка если делаем страницу авторизации
     оставляем значение false, если занимаеся другими, ставим true
   */
-  const isAuthenticated = false;
+  const isAuthenticated = !!token;
 
   const routes = useRouts(isAuthenticated);
 
+  if (!ready) {
+    console.log('loading');
+  }
+
   return (
-    <Router>
-      <div className="container">
-        {routes}
-      </div>
-    </Router>
+    <AuthContext.Provider
+      value={{
+        token, login, logout, userId, isAuthenticated,
+      }}
+    >
+      <Router>
+        <div className="container">
+          {routes}
+        </div>
+      </Router>
+    </AuthContext.Provider>
   );
 }
 
