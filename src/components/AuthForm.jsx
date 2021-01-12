2517e8abf3b2d502eec73244b8d3f6804b4e622e
Yuliya Reutovich <yrevtovich@mail.ru>
2021-01-12 23:45:18 +0300

    feat: add authorization form markup

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -1,13 +1,41 @@
+/* eslint-disable react/jsx-one-expression-per-line */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable react/jsx-closing-tag-location */
+/* eslint-disable react/jsx-indent */
+/* eslint-disable no-multi-spaces */
+/* eslint-disable no-unused-vars */
 import {
   Box,
   Grid,
   TextField,
   Button,
+  Container,
+  Paper,
+  Tabs,
+  Tab,
 } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import React, { useState, useEffect, useContext } from 'react';
 import AuthContext from '../context/AuthContext';
 import useHttp from '../hooks/http.hook';
 // import useStyles from '../hooks/style.hook';
+
+const useStyles = makeStyles((theme) => ({
+  auth__form: {
+    padding: '0 10px 30px',
+    maxWidth: '350px',
+    borderRadius: '5px',
+    boxShadow: `
+      0 0 10px ${theme.palette.shadow.main}, 
+      0 0 10px ${theme.palette.shadow.main}, 
+      0 0 10px ${theme.palette.shadow.main}
+    `,    
+  },
+  auth__tabs: {
+    maxWidth: '50%',
+    minWidth: '40%',
+  },
+}));
 
 const AuthForm = () => {
   const [form, setForm] = useState({
@@ -15,6 +43,8 @@ const AuthForm = () => {
     password: '',
     fullName: '',
   });
+
+  const classes = useStyles();
 
   const {
     loading,
@@ -64,47 +94,92 @@ const AuthForm = () => {
     setForm({ ...form, [event.target.name]: event.target.value });
   };
 
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
-    <Grid item xs={6}>
-      <Grid container bgcolor="green accent-4" color="secondary.contrastText" p={2}>
+    <Grid
+      container
+      className={classes.auth__form}
+      direction="column"
+      // justifyContent="center"
+      alignItems="stretch"
+      spacing={3}
+    >
+      <Grid item xs={12}>
+        <Tabs
+          variant="fullWidth"
+          value={value}
+          indicatorColor="primary"
+          textColor="inherit"
+          onChange={handleChange}
+          scrollButtons="off"
+          // aria-label="disabled tabs example"
+        >
+          <Tab
+            label="Sing Up" 
+            className={classes.auth__tabs}
+          />
+          <Tab
+            label="Login"           
+            className={classes.auth__tabs}
+          />
+        </Tabs>
+      </Grid>
+      {!value && (
+        <Grid item xs={12}>
+          <TextField
+            required
+            fullWidth
+            size="small"
+            variant="outlined"
+            label="Name"
+            type="text"
+            name="fullName"
+            onChange={changeHandler}
+          />
+        </Grid>
+      )}
+      <Grid item xs={12}>          
         <TextField
-          label="Standard"
-          placeholder="Email"
+          required
+          fullWidth
+          size="small"
+          variant="outlined"
+          label="Email"
           type="email"
           name="email"
           onChange={changeHandler}
         />
+      </Grid>        
+      <Grid item xs={12}>
         <TextField
-          label="Standard"
-          placeholder="Full name"
-          type="text"
-          name="fullName"
-          onChange={changeHandler}
-        />
-        <TextField
-          label="Standard"
-          placeholder="Password"
+          required
+          fullWidth
+          size="small"
+          variant="outlined"
+          label="Password"
           type="password"
           name="password"
+          helperText="Incorrect entry."
+          error={false} /* -----------------   */
           onChange={changeHandler}
         />
       </Grid>
-      <Box>
+      <Grid item xs={5}>
         <Button
           variant="contained"
-          onClick={registerHandler}
+          onClick={!value ? registerHandler : loginHandler}
           disabled={loading}
+          fullWidth
+          color="primary"
         >
-          Singup
+          {!value ? 'Sign Up' : 'Login'}
         </Button>
-        <Button
-          variant="contained"
-          onClick={loginHandler}
-          disabled={loading}
-        >
-          Login
-        </Button>
-      </Box>
+      </Grid>
     </Grid>
   );
 };
--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -1,12 +1,30 @@
 import {
+  // CssBaseline,
   Container,
-  Grid,
+  // Grid,
   Typography,
 } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import React from 'react';
 import AuthForm from '../components/AuthForm';
-import useStyles from '../hooks/style.hook';
 import vars from '../variables';
+
+const useStyles = makeStyles((theme) => ({
+  auth: {
+    padding: '32px',
+    minHeight: '100vh',
+    backgroundColor: theme.palette.background.main,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    boxSizing: 'border-box',
+    flexDirection: 'column',
+  },
+  auth__title: {
+    marginBottom: '80px',
+    textAlign: 'center',
+  },
+}));
 
 const AuthPage = () => {
   const title = `Welcome to ${vars.APP_NAME}`;
@@ -14,18 +32,11 @@ const AuthPage = () => {
   const classes = useStyles();
 
   return (
-    <Container>
+    <Container className={classes.auth} maxWidth={false}>
       <Typography className={classes.auth__title} variant="h1">
         {title}
       </Typography>
-      <Grid
-        container
-        direction="row"
-        justify="center"
-        alignItems="center"
-      >
-        <AuthForm />
-      </Grid>
+      <AuthForm />
     </Container>
   );
 };
