c10c09a625f3c77ab9937bd4c85bc91594ee7f91
Yuliya Reutovich <yrevtovich@mail.ru>
2021-01-14 18:08:35 +0300

    refactor: separate AuthForm module to modules AuthCard, AuthForm, AuthFormTabs

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -1,12 +1,9 @@
-import {
-  Container,
-  Typography,
-  Paper,
-} from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
 import React from 'react';
+import { Container, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import { SnackbarProvider } from 'notistack';
-import AuthForm from '../components/AuthForm';
+import AuthCard from '../components/AuthCard/AuthCard';
+import ValidationRequirements from '../components/ValidationRequirements/ValidationRequirements';
 import vars from '../variables';
 
 const useStyles = makeStyles((theme) => ({
@@ -24,70 +21,28 @@ const useStyles = makeStyles((theme) => ({
     marginBottom: '60px',
     textAlign: 'center',
   },
-  auth__requirements: {
-    padding: '20px',
-    maxWidth: '800px',
-    '& h2': {
-      marginBottom: '15px',
-    },
-    '& h3': {
-      marginBottom: '5px',
-    },
-    '& p': {
-      marginBottom: '10px',
-      textAlign: 'justify',
-    },
-  },
 }));
 
 const AuthPage = () => {
-  const title = `Welcome to ${vars.APP_NAME}`;
-
   const classes = useStyles();
 
   return (
-    <SnackbarProvider
-      maxSnack={3}
-      autoHideDuration={2000}
-      anchorOrigin={{
-        vertical: 'bottom',
-        horizontal: 'right',
-      }}
-    >
-      <Container className={classes.auth} maxWidth={false}>
-        <Typography className={classes.auth__title} variant="h1">
-          {title}
-        </Typography>
-        <AuthForm />
-        <Paper elevation={3} className={classes.auth__requirements}>
-          <Typography variant="h2">
-            Validation requirements
-          </Typography>
-          <Typography variant="h3">
-            Login
-          </Typography>
-          <Typography variant="body2">
-            Only latin alphabet, numbers and special symbols.
-          </Typography>
-          <Typography variant="h3">
-            Email
-          </Typography>
-          <Typography variant="body2">
-            It should contain only one &quot;@&quot; symbol, at least one latin letter or
-            number before &quot;@&quot;, only one &quot;.&quot; after &quot;@&quot;,
-            at least one latin letter or number after &quot;@&quot;
-            and before &quot;.&quot;,  at least two symbols after &quot;.&quot;.
-          </Typography>
-          <Typography variant="h3">
-            Password
-          </Typography>
-          <Typography variant="body2">
-            It should contain at least 6 symbols, one uppercase latin symbol,
-            1 lowercase latin symbol, 1 number.
-          </Typography>
-        </Paper>
-      </Container>
-    </SnackbarProvider>
+    <Container className={classes.auth} maxWidth={false}>
+      <Typography className={classes.auth__title} variant="h1">
+        {`Welcome to ${vars.APP_NAME}`}
+      </Typography>
+      <SnackbarProvider
+        maxSnack={3}
+        autoHideDuration={2000}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'right',
+        }}
+      >
+        <AuthCard />
+      </SnackbarProvider>
+      <ValidationRequirements />
+    </Container>
   );
 };
 
--- src/variables.js
+++ src/variables.js
@@ -1,3 +1,6 @@
 export default {
   APP_NAME: 'RS Tasktracker',
+  FULLNAME_REGEXP: /^[\w!#$%&'*+/=?^_`{|}~\- №"@]+$/,
+  EMAIL_REGEXP: /^[\w!#$%&'*+/=?^_`{|}~]*(?:\.?[\w!#$%&'*+/=?^_`{|}~-]+)@[^.@]+\.[^.@]{2,}$/,
+  PASSWORD_REGEXP: /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{6,}$/,
 };
